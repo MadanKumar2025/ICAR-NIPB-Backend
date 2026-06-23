4b2f7bdbde58774ed93d82daaf0ca6bdae16bdde
gauravgurjar344 <gauravgurjar.cse19@chitkarauniversity.edu.in>
2026-06-23 10:20:46 +0530

    displayOrderNo new update

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -1,49 +1,3 @@
-// import multer from "multer";
-// import path from "path";
-// import fs from "fs";
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     // cb(null, "uploads/");
-//     const uploadDir = "uploads/";
-//     if (!fs.existsSync(uploadDir)) {
-//       fs.mkdirSync(uploadDir);
-//     }
-
-//     cb(null, uploadDir);
-//   },
-//   filename: (req, file, cb) => {
-//     const uniqueName =
-//       Date.now() +
-//       "-" +
-//       Math.round(Math.random() * 1e9) +
-//       path.extname(file.originalname);
-
-//     cb(null, uniqueName);
-//   },
-// });
-
-// const fileFilter = (req, file, cb) => {
-//   const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
-
-//   if (allowedTypes.includes(file.mimetype)) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error("Only JPG, PNG, WEBP images are allowed"), false);
-//   }
-// };
-
-// const upload = multer({
-//   storage,
-//   limits: {
-//     fileSize: 5 * 1024 * 1024,
-//   },
-//   fileFilter,
-// });
-
-// export default upload;
-
-
 import multer from "multer";
 import path from "path";
 import fs from "fs";
@@ -70,12 +24,12 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp"];
+  const allowedTypes = ["image/jpeg", "image/png", "image/jpg", "image/webp","application/pdf"];
 
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
   } else {
-    cb(new Error("Only JPG, PNG, WEBP images are allowed"), false);
+    cb(new Error("Only JPG, PNG, WEBP images and Pdf are allowed"), false);
   }
 };
 
