22d3a4f4c1df75b901f3a78c1062b2eb159f9f94
gauravgurjar344 <gauravgurjar.cse19@chitkarauniversity.edu.in>
2026-07-07 16:34:18 +0530

    update Banner

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
-// import express from "express";
-// import { loginUser } from "../controllers/authController.js";
+import express from "express";
+import { loginUser,forgotPassword } from "../controllers/authController.js";
 
-// const router = express.Router();
+const router = express.Router();
 
-// router.route('/login').post(loginUser)
-// export default router;
+router.route('/login').post(loginUser);
+router.post("/forgot-password", forgotPassword);
+export default router;
 
 import express from "express";
 import { loginUser, forgotPassword } from "../controllers/authController.js";
