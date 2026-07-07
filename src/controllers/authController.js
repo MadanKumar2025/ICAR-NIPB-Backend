fe71c4d0429e240aadf24476552a3b45b4022a68
gauravgurjar344 <gauravgurjar.cse19@chitkarauniversity.edu.in>
2026-07-07 15:38:48 +0530

    update Banner

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 dotenv.config();
+import nodemailer from "nodemailer";
 
 const JWT_SECRET = process.env.JWT_SECRET || "fallback_secret_key";
 
@@ -51,5 +52,56 @@ export const loginUser = async (req, res) => {
       success: false,
       message: error.message,
     });
+  }
+};
+
+
+export const forgotPassword = async (req, res) => {
+  try {
+
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Email not found",
+      });
+    }
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: user.email,
+      subject: "Forgot Password",
+      html: `
+        <h2>Hello ${user.name}</h2>
+
+        <p>Your Password is:</p>
+
+        <h3>${user.password}</h3>
+      `,
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Password sent successfully.",
+    });
+
+  } catch (error) {
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+
   }
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,18 @@
+// import express from "express";
+// import { loginUser } from "../controllers/authController.js";
+
+// const router = express.Router();
+
+// router.route('/login').post(loginUser)
+// export default router;
+
 import express from "express";
-import { loginUser } from "../controllers/authController.js";
+import { loginUser, forgotPassword } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.route('/login').post(loginUser)
+router.post("/login", loginUser);
+
+router.post("/forgot-password", forgotPassword);
+
 export default router;
